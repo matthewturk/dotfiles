62dcd885ba197cd96864e21fae414cd0a025311d
Matthew Turk <matthewturk@gmail.com>
2015-08-21 15:02:08 -0500

    Enh, don't need the newscreen one.

--- spawn_gotty_newscreen.sh
+++
@@ -1,2 +0,0 @@
-session_name=`date -Is`
-screen -t 'shared-full' /home/mturk/Development/go/bin/gotty -r screen -S gotty-${session_name}
--- spawn_gotty_screen.sh
+++ spawn_gotty_screen.sh
@@ -1,3 +1,4 @@
 echo -n "Which screen terminal to cast? "
 read term
-screen -t 'shared' /home/mturk/Development/go/bin/gotty -r screen -x IRC -p ${term}
+ip_address=`hostname -I|cut -f1 -d\ `
+screen -t 'shared' /home/mturk/Development/go/bin/gotty -a ${ip_address} -r screen -x IRC -p ${term}
