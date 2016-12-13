91cbc8182a2d6671511886231d12270a12aba94f
Matthew Turk <matthewturk@gmail.com>
2016-12-13 09:21:41 -0600

    Updating hgrc to point to dotfiles, adding a setup script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# This is the simple setup script that updates everything.
+
+cd $HOME/dotfiles/
+hg clone https://bitbucket.org/durin42/hg-remotebranches
+hg clone https://bitbucket.org/marmoute/mutable-history
+hg clone https://bitbucket.org/MatthewTurk/hgbb
