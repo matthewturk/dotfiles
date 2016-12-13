6680b41665dd4a9c0f32462e254e70e99d4a03e7
Matthew Turk <matthewturk@gmail.com>
2016-12-13 09:24:59 -0600

    Updating setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,13 @@ cd $HOME/dotfiles/
 hg clone https://bitbucket.org/durin42/hg-remotebranches
 hg clone https://bitbucket.org/marmoute/mutable-history
 hg clone https://bitbucket.org/MatthewTurk/hgbb
+
+cd $HOME
+
+for rc in screenrc hgrc vimrc zshrc xonshrc
+do
+  echo ln -s $HOME/dotfiles/${rc} .${rc}
+done
+ln -s $HOME/dotfiles/vim .vim
+mkdir -p $HOME/.config/xonsh
+ln -s $HOME/dotfiles/xonsh-config.json $HOME/.config/xonsh/config.json
