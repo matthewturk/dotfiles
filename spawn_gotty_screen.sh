ba2f9eda9dd03a3c1f8d9afee5b059531240ed9d
Matthew Turk <matthewturk@gmail.com>
2021-05-28 06:55:37 -0500

    Trying to match chezmoi

--- spawn_gotty_screen.sh
+++
@@ -1,4 +0,0 @@
-echo -n "Which screen terminal to cast? "
-read term
-ip_address=`hostname -I|cut -f1 -d\ `
-screen -t 'shared' /home/mturk/Development/go/bin/gotty -a ${ip_address} -r screen -x IRC -p ${term}
