37b185ea3a66fd368aa6be0f1e1fdf997da0930d
Matthew Turk <matthewturk@gmail.com>
2026-08-14 08:27:12 -0500

    Update .local/bin/i3-keys.sh

--- dot_local/bin/executable_i3-keys.sh
+++ dot_local/bin/executable_i3-keys.sh
@@ -2,9 +2,22 @@
 
 CONFIG_FILE="${XDG_CONFIG_HOME:-$HOME/.config}/i3/config"
 
-grep -E '^\s*bind(sym|code)' "$CONFIG_FILE" \
+# 1. Parse the config: Squeeze spaces, strip keywords/flags, and swap $mod for Super
+list=$(grep -E '^\s*bind(sym|code)' "$CONFIG_FILE" \
+    | tr -s ' ' \
     | sed -E 's/^\s*bind(sym|code)\s+//' \
-    | sed -E 's/--no-startup-id\s+//' \
     | sed -E 's/--release\s+//' \
-    | sed -E "s/\\\$mod/Super/g" \
-    | rofi -dmenu -i -p "i3 Shortcuts" -l 15
+    | sed -E 's/--no-startup-id\s+//' \
+    | sed -E 's/\$mod/Super/g')
+
+# 2. Pipe to rofi and store the user's choice in a variable
+selection=$(echo "$list" | rofi -dmenu -i -p "Run Shortcut" -l 15)
+
+# 3. If the user picked something (didn't press Escape), execute it
+if [ -n "$selection" ]; then
+    # Extract everything *after* the first space (which isolates the command)
+    command=$(echo "$selection" | cut -d' ' -f2-)
+    
+    # Pass the command to i3 to execute
+    i3-msg "$command"
+fi
